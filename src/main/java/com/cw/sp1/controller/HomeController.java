d05b5b9ba5206348583f43c248602dd2761f08fa
cheongwoo <skacjddn7@gmail.com>
2023-10-20 12:26:53 +0900

    feat: add 회원 웹 기능 - 홈화면, 등록, 조회

---
+++ src/main/java/com/cw/sp1/controller/HomeController.java
@@ -0,0 +1,46 @@
+package com.cw.sp1.controller;
+
+import com.cw.sp1.domain.Member;
+import com.cw.sp1.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    private final MemberService memberService;
+
+    public HomeController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+
+    @GetMapping("/members/new")
+    public String creatForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String creat(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+}
